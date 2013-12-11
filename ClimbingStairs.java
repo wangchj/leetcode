3455807214ab236b2ac30658a612b62ad746b0eb
Jeff Wang <cjw39@hotmail.com>
2013-12-11 01:06:52 -0600

    Move cached answer outside of climbStairs() so the array is not constructed every call.

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -8,19 +8,20 @@
 */
 public class ClimbingStairs
 {
-   public int climbStairs(int n)
-   {
-      int[] ans = {
-         1, 2, 3, 5, 8, 13, 21, 34, 55, 89,
-         144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946,
-         17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269
-      };
+    int[] ans = {
+        1, 2, 3, 5, 8, 13, 21, 34, 55, 89,
+        144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946,
+        17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269
+    };
+    
+    public int climbStairs(int n)
+    {
       
-      if(n <= ans.length)
-         return ans[n-1];
+        if(n <= ans.length)
+            return ans[n-1];
       
-      int result = climbStairs(n - 1);
-      result += climbStairs(n - 2);
-      return result;
-   }
+        int result = climbStairs(n - 1);
+        result += climbStairs(n - 2);
+        return result;
+    }
 }
